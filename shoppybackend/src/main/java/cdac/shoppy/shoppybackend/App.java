5306d98b7e5f0d0f304af019491ab6b088ee384e
Akshay Meshram <akshaymeshram4@gmail.com>
2019-01-19 12:18:40 +0530

    my project setup completed successfully.

---
+++ shoppybackend/src/main/java/cdac/shoppy/shoppybackend/App.java
@@ -0,0 +1,13 @@
+package cdac.shoppy.shoppybackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
